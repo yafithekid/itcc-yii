0aa529756c0c161ae85a9ac691a140de9ee833e2
opelhoward <opelhoward@yahoo.com>
2014-10-31 18:54:03 +0700

    Change design
    New Design

--- views/layouts/sidebar.php
+++ views/layouts/sidebar.php
@@ -13,72 +13,83 @@ $last_login_users = User::find()->where(['is_online'=>0])->orderBy(['last_login'
 <?php $this->beginContent('@app/views/layouts/main.php'); ?>
     <div class="row">
         <div class="col-md-3 sidebar">
-            <?php if (isset(Yii::$app->user->identity)):?>
-            <?= '<h5>Menu Mahasiswa</h5>'; ?>
-            <?= Nav::widget([
-                    'options' => ['class' => 'nav nav-sidebar'],
-                    'items' => [
-                        ['label' => '<span class="glyphicon glyphicon-user"></span> Ubah Profil', 'url' => ['/user/update','id'=>Yii::$app->user->identity->id]],
-                        ['label' => '<span class="glyphicon glyphicon-envelope"></span> Pesan', 'url' => ['/message/index']],
-                        ['label' => '<span class="glyphicon glyphicon-book"></span> Daftar Kuliah Tersedia', 'url' => ['/course/index']],
-                        ['label' => '<span class="glyphicon glyphicon-pencil"></span> Tugas', 'url' => ['/task/student','id'=>Yii::$app->user->identity->id]],
-                    ],
-                    'encodeLabels'=>false,
+            <div id="accordion">
+                <?php if (isset(Yii::$app->user->identity)):?>
+                    <?= '<h5><a href=#>Menu Mahasiswa</a></h5>'; ?>
+                    <?= Nav::widget([
+                            'options' => ['class' => 'nav nav-sidebar'],
+                            'items' => [
+                                ['label' => '<span class="glyphicon glyphicon-user"></span> Ubah Profil', 'url' => ['/user/update','id'=>Yii::$app->user->identity->id]],
+                                ['label' => '<span class="glyphicon glyphicon-envelope"></span> Pesan', 'url' => ['/message/index']],
+                                ['label' => '<span class="glyphicon glyphicon-book"></span> Daftar Kuliah Tersedia', 'url' => ['/course/index']],
+                                ['label' => '<span class="glyphicon glyphicon-pencil"></span> Tugas', 'url' => ['/task/student','id'=>Yii::$app->user->identity->id]],
+                            ],
+                            'encodeLabels'=>false,
 
-                ]);
-            ?>
-            <?php endif; ?>
-            <?php
-                if(isset(Yii::$app->user->identity) && (Yii::$app->user->identity->isTeacher())) {
-                    echo '<h5>Menu Dosen</h5>';
-                    echo Nav::widget([
-                        'options' => ['class' => 'nav nav-sidebar'],
-                        'items' => [
-                            ['label' => '<span class="glyphicon glyphicon-book"></span> Kuliah Saya', 'url' => ['/course/teacher']],
-                            ['label' => '<span class="glyphicon glyphicon-pencil"></span> Tugas Saya', 'url' => ['/task/teacher']],
-                        ],
-                        'encodeLabels' => false,
-                    ]);
-                }
-            ?>
-            <?php
-                if(isset(Yii::$app->user->identity) && (Yii::$app->user->identity->isAdmin())) {
-                    echo '<h5>Menu Admin</h5>';
-                    echo Nav::widget([
-                        'options' => ['class' => 'nav nav-sidebar'],
-                        'items' => [
-                            ['label' => '<span class="glyphicon glyphicon-user"></span> Pengguna', 'url' => ['/user/index']],
-                            ['label' => 'Departemen', 'url' => ['/department/index']],
-                            ['label' => 'Fakultas', 'url' => ['/faculty/index']],
-                        ],
-                        'encodeLabels' => false,
-                    ]);
-                }
-            ?>
+                        ]);
+                    ?>
+                <?php endif; ?>
+                <?php
+                    if(isset(Yii::$app->user->identity) && (Yii::$app->user->identity->isTeacher())) {
+                        echo '<h5><a href=#>Menu Dosen</a></h5>';
+                        echo Nav::widget([
+                            'options' => ['class' => 'nav nav-sidebar'],
+                            'items' => [
+                                ['label' => '<span class="glyphicon glyphicon-book"></span> Kuliah Saya', 'url' => ['/course/teacher']],
+                                ['label' => '<span class="glyphicon glyphicon-pencil"></span> Tugas Saya', 'url' => ['/task/teacher']],
+                            ],
+                            'encodeLabels' => false,
+                        ]);
+                    }
+                ?>
+                <?php
+                    if(isset(Yii::$app->user->identity) && (Yii::$app->user->identity->isAdmin())) {
+                        echo '<h5><a href=#>Menu Admin</a></h5>';
+                        echo Nav::widget([
+                            'options' => ['class' => 'nav nav-sidebar'],
+                            'items' => [
+                                ['label' => '<span class="glyphicon glyphicon-user"></span> Pengguna', 'url' => ['/user/index']],
+                                ['label' => 'Departemen', 'url' => ['/department/index']],
+                                ['label' => 'Fakultas', 'url' => ['/faculty/index']],
+                            ],
+                            'encodeLabels' => false,
+                        ]);
+                    }
+                ?>
+            </div>
         </div>
-
         <div id="main-content" class="col-md-9">
-            <?= Breadcrumbs::widget([
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            ]) ?>
             <div class="col-md-8"><?= $content ?></div>
            
             <div class="col-md-4">
-                <div class="well well-sm">
-                    <h5><span class='glyphicon glyphicon-globe'></span> Pengguna online</h5>
+                <h5><span class='glyphicon glyphicon-globe'></span> Pengguna online</h5>
+                <div class="sidebar-kanan">
                     <?php foreach ($online_users as $user): ?>
                         <?= $user->fullname."<br>"; ?>
                     <?php endforeach; ?>
                 </div>
-                <div class="well well-sm">
-                    <h5><span class='glyphicon glyphicon-time'></span> Pengguna login</h5>
+                <h5><span class='glyphicon glyphicon-time'></span> Pengguna login</h5>
+                <div class="sidebar-kanan">
                     <?php
                         foreach ($last_login_users as $user): ?>
                         <?= $user->fullname."<br>"; ?>
                     <?php endforeach;?>
                 </div>
             </div>
-            
         </div>
+         <script>
+            window.onload = function(){
+                $("#accordion h5").next().hide();
+                $("#accordion h5").click(function() {
+                    $(this).next().slideToggle();
+                });
+            };
+        </script>
+        <style type="text/css">
+            #accordion h5 {
+                background-color: #f5f5f5;
+                padding: 5px;
+            }
+        </style>
     </div>
 <?php $this->endContent(); ?>
--- views/site/login.php
+++ views/site/login.php
@@ -9,38 +9,37 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-    
-    <p>Please fill out the following fields to login:</p>
+<div style="background-image: url('<?=Yii::$app->request->baseUrl;?>/img/duo-reading.jpg'); width:100%; display:table; ">
+    <div class="site-login">
+        <h1><?= Html::encode($this->title) ?></h1>
+        
+        <p>Please fill out the following fields to login:</p>
 
-    <?php $form = ActiveForm::begin([
-        'id' => 'login-form',
-        'options' => ['class' => 'form-horizontal'],
-        'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-1 control-label'],
-        ],
-    ]); ?>
+        <?php $form = ActiveForm::begin([
+            'id' => 'login-form',
+            'options' => ['class' => 'form-horizontal'],
+            'fieldConfig' => [
+                'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+                //'labelOptions' => ['class' => 'col-lg-1 control-label'],
+            ],
+        ]); ?>
+        <?= Html::activeLabel($model,'username'); ?>
+        <?= Html::activeTextInput($model,'username',['class'=>'form-control']); ?>
+        <?php //$form->field($model, 'username') ?>
+        <?= Html::activeLabel($model,'password'); ?>
+        <?= Html::activePasswordInput($model,'password',['class'=>'form-control']); ?>
+        <?php //$form->field($model, 'password')->passwordInput() ?>
 
-    <?= $form->field($model, 'username') ?>
+        <?= $form->field($model, 'rememberMe', [
+            'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
+        ])->checkbox() ?>
 
-    <?= $form->field($model, 'password')->passwordInput() ?>
-
-    <?= $form->field($model, 'rememberMe', [
-        'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-    ])->checkbox() ?>
-
-    <div class="form-group">
-        <div class="col-lg-offset-1 col-lg-11">
-            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+        <div class="form-group">
+            <div class="col-lg-offset-1 col-lg-11">
+                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            </div>
         </div>
-    </div>
 
-    <?php ActiveForm::end(); ?>
-
-    <div class="col-lg-offset-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
+        <?php ActiveForm::end(); ?>
     </div>
 </div>
