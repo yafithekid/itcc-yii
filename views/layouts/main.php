92d4de5b6cf33e5202416702cdb958d884908fca
opelhoward <opelhoward@yahoo.com>
2014-10-31 15:18:23 +0700

    WOOHOO

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -48,10 +48,10 @@ AppAsset::register($this);
             NavBar::end();
         ?>
 
-        <div class="container">
             <?= Breadcrumbs::widget([
                 'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
             ]) ?>
+        <div class="container">
             <?= $content ?>
         </div>
     </div>
